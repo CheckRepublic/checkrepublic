6f9180e9bd3b5c0fedcc38a2b53177e78e4c16bd
dennis <dp.jandow@gmail.com>
2024-11-23 15:03:22 +0100

    fuck you

--- db/postgres_db.go
+++ db/postgres_db.go
@@ -20,6 +20,18 @@ func InitPostgres() {
 	DB = PostgresDB{Db: db}
 	log.Info("Database created")
 
+	// Create car types enum
+	_, err = db.Exec(context.Background(), `
+	DO $$ BEGIN
+CREATE TYPE car_type_enum AS ENUM ('small', 'sports', 'luxury', 'family');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Create table
 	_, err = db.Exec(context.Background(), `
 		CREATE TABLE IF NOT EXISTS offers
@@ -32,7 +44,7 @@ func InitPostgres() {
     end_date        bigint   not null,
     number_seats    smallint not null,
     price           integer  not null,
-    car_type        text     not null,
+    car_type        text not null,
     has_vollkasko   boolean  not null,
     free_kilometers smallint not null
 );`)
@@ -112,31 +124,149 @@ func (p PostgresDB) GetAllOffers(ctx context.Context) models.Offers {
 	}
 }
 
-func (p PostgresDB) GetFilteredOffers(ctx context.Context, regionID uint64, timeRangeStart uint64, timeRangeEnd uint64, numberDays uint64, sortOrder string, page uint64, pageSize uint64, priceRangeWidth uint64, minFreeKilometerWidth uint64, minNumberSeats *uint64, minPrice *uint64, maxPrice *uint64, carType *string, onlyVollkasko *bool, minFreeKilometer *uint64) models.Offers {
+func (p PostgresDB) GetFilteredOffers(ctx context.Context, regionID uint64, timeRangeStart uint64, timeRangeEnd uint64, numberDays uint64, sortOrder string, page uint64, pageSize uint64, priceRangeWidth uint64, minFreeKilometerWidth uint64, minNumberSeats *uint64, minPrice *uint64, maxPrice *uint64, carType *string, onlyVollkasko *bool, minFreeKilometer *uint64) (dto models.DTO) {
 	// Query for offers
-	query := `
+	// filteredMandatoryQuery := `
+	// 	SELECT *
+	// 	FROM offers
+	// 	WHERE region_id = any($1)  -- Match the region or any of its subregions
+	// 	AND start_date >= $2 
+	// 	AND end_date <= $3
+	// 	AND (end_date - start_date) / 86400000 >= $4  -- The number of full days (24h) the car is available within the rangeStart and rangeEnd
+	// 	`
+	// filteredMandatoryQuery = filteredMandatoryQuery
+
+	priceRangeCount := `
+	WITH offers AS (
 		SELECT *
 		FROM offers
 		WHERE region_id = any($1)  -- Match the region or any of its subregions
-		AND start_date >= $2 AND end_date <= $3
-		AND ($4::int IS NULL OR number_seats >= $4)
-		AND ($5::int IS NULL OR price >= $5)
-		AND ($6::int IS NULL OR price < $6)
-		AND ($7::text IS NULL OR car_type = $7)
-		AND (NOT $8 OR has_vollkasko = true)
-		AND ($9::int IS NULL OR free_kilometers >= $9)
+		AND start_date >= $2 
+		AND end_date <= $3
+		AND (end_date - start_date) / 86400000 >= $4  -- The number of full days (24h) the car is available within the rangeStart and rangeEnd
+	),
+	price_ranges AS (
+		SELECT * from generate_series(0, (SELECT max(price) FROM offers), $5) AS range_start
+	)
+
+    SELECT
+        pr.range_start,
+		pr.range_start + $5 AS range_end,
+        COUNT(*) AS item_count
+    FROM
+        price_ranges pr
+    INNER JOIN
+        offers t ON t.price >= pr.range_start AND t.price < pr.range_start + $5
+    GROUP BY
+        pr.range_start
+`
+
+	carTypeCountQuery := `
+	WITH offers AS (
+		SELECT *
+		FROM offers
+		WHERE region_id = any($1)  -- Match the region or any of its subregions
+		AND start_date >= $2 
+		AND end_date <= $3
+		AND (end_date - start_date) / 86400000 >= $4  -- The number of full days (24h) the car is available within the rangeStart and rangeEnd
+	)
+			SELECT
+				car_type,
+				count(*) as count
+			FROM offers
+					WHERE $5
+
+			GROUP BY car_type`
+	seatsCount := `
+	WITH offers AS (
+		SELECT *
+		FROM offers
+		WHERE region_id = any($1)  -- Match the region or any of its subregions
+		AND start_date >= $2 
+		AND end_date <= $3
+		AND (end_date - start_date) / 86400000 >= $4  -- The number of full days (24h) the car is available within the rangeStart and rangeEnd
+	)
+			SELECT
+				number_seats,
+				count(*) as count
+			FROM offers
+					WHERE $5
+
+			GROUP BY number_seats
+		`
+	freeKilometerRange := `
+	WITH offers AS (
+		SELECT *
+		FROM offers
+		WHERE region_id = any($1)  -- Match the region or any of its subregions
+		AND start_date >= $2 
+		AND end_date <= $3
+		AND (end_date - start_date) / 86400000 >= $4  -- The number of full days (24h) the car is available within the rangeStart and rangeEnd
+	),
+	free_kilometer_ranges AS (
+    SELECT generate_series(0, (SELECT max(free_kilometers) FROM offers), $6) AS range_start
+	)
+
+    SELECT
+        fkr.range_start,
+		fkr.range_start + $6 AS range_end,
+        COUNT(*) AS item_count
+    FROM
+        free_kilometer_ranges fkr
+    INNER JOIN
+        offers t ON t.free_kilometers >= fkr.range_start AND t.free_kilometers < fkr.range_start + $6
+	WHERE $5
+    GROUP BY
+        fkr.range_start
+`
+	vollkaskoCountQuery := `
+	WITH offers AS (
+		SELECT *
+		FROM offers
+		WHERE region_id = any($1)  -- Match the region or any of its subregions
+		AND start_date >= $2 
+		AND end_date <= $3
+		AND (end_date - start_date) / 86400000 >= $4  -- The number of full days (24h) the car is available within the rangeStart and rangeEnd
+	)
+			SELECT
+				has_vollkasko,
+				count(*) as count
+			FROM offers
+					WHERE $5
+
+			GROUP BY has_vollkasko`
+
+	orderedAndPaginated := `
+	WITH offers AS (
+		SELECT *
+		FROM offers
+		WHERE region_id = any($1)  -- Match the region or any of its subregions
+		AND start_date >= $2 
+		AND end_date <= $3
+		AND (end_date - start_date) / 86400000 >= $4  -- The number of full days (24h) the car is available within the rangeStart and rangeEnd
+	)
+			SELECT *
+			FROM offers
+			WHERE ($6::int IS NULL OR number_seats >= $6)
+			AND $5
+		AND ($7::int IS NULL OR price >= $7)
+		AND ($8::int IS NULL OR price < $8)
+		AND ($9::text IS NULL OR car_type = $9)
+		AND (NOT $10 OR has_vollkasko = true)
+		AND ($11::int IS NULL OR free_kilometers >= $11)
 		ORDER BY price ` + parseSortOrder(sortOrder) + `
-		LIMIT $10 OFFSET $11
-    `
+		LIMIT $12 OFFSET $13  `
 
 	// Pagination offset
 	offset := (page - 1) * pageSize
 
 	// Query execution
-	rows, err := p.Db.Query(ctx, query,
+	rows, err := p.Db.Query(ctx, orderedAndPaginated,
 		regionIdToMostSpecificRegionId[regionID],
 		timeRangeStart,
 		timeRangeEnd,
+		numberDays,
+		true,
 		minNumberSeats,
 		minPrice,
 		maxPrice,
@@ -148,7 +278,7 @@ func (p PostgresDB) GetFilteredOffers(ctx context.Context, regionID uint64, time
 	)
 	if err != nil {
 		log.Error("Unable to fetch offers", err)
-		return models.Offers{}
+		return dto
 	}
 	defer rows.Close()
 
@@ -158,7 +288,134 @@ func (p PostgresDB) GetFilteredOffers(ctx context.Context, regionID uint64, time
 		log.Error("Unable to collect offers: %v\n", err)
 	}
 
-	return models.Offers{Offers: offers}
+	// collect price range
+	rows, err = p.Db.Query(ctx, priceRangeCount, regionIdToMostSpecificRegionId[regionID],
+		timeRangeStart,
+		timeRangeEnd,
+		numberDays,
+		priceRangeWidth)
+	if err != nil {
+		log.Error("Unable to fetch price range", err)
+		return dto
+	}
+	defer rows.Close()
+
+	var priceRanges []models.PriceRange
+	for rows.Next() {
+		var priceRange models.PriceRange
+		err = rows.Scan(&priceRange.Start, &priceRange.End, &priceRange.Count)
+		if err != nil {
+			log.Error("Unable to scan price range", err)
+			return dto
+		}
+		priceRanges = append(priceRanges, priceRange)
+	}
+
+	// collect car type count
+	rows, err = p.Db.Query(ctx, carTypeCountQuery, regionIdToMostSpecificRegionId[regionID],
+		timeRangeStart,
+		timeRangeEnd,
+		numberDays,
+		true)
+	if err != nil {
+		log.Error("Unable to fetch car type count", err)
+		return dto
+	}
+	defer rows.Close()
+
+	carTypeCountMap := make(map[string]uint64)
+	for rows.Next() {
+		var carType string
+		var count uint64
+		err = rows.Scan(&carType, &count)
+		if err != nil {
+			log.Error("Unable to scan car type count", err)
+			return dto
+		}
+		carTypeCountMap[carType] = count
+	}
+
+	var carTypeCount models.CarTypeCount
+	carTypeCount.Small = carTypeCountMap["small"]
+	carTypeCount.Sports = carTypeCountMap["sports"]
+	carTypeCount.Luxury = carTypeCountMap["luxury"]
+	carTypeCount.Family = carTypeCountMap["family"]
+
+	// collect seats count
+	rows, err = p.Db.Query(ctx, seatsCount, regionIdToMostSpecificRegionId[regionID],
+		timeRangeStart,
+		timeRangeEnd,
+		numberDays,
+		true)
+	if err != nil {
+		log.Error("Unable to fetch seats count", err)
+		return dto
+	}
+	defer rows.Close()
+
+	var seatsCountList []models.SeatsCount
+	for rows.Next() {
+		var seatsCount models.SeatsCount
+		err = rows.Scan(&seatsCount.NumberSeats, &seatsCount.Count)
+		if err != nil {
+			log.Error("Unable to scan seats count", err)
+			return dto
+		}
+		seatsCountList = append(seatsCountList, seatsCount)
+	}
+
+	// collect free kilometer range
+	rows, err = p.Db.Query(ctx, freeKilometerRange, regionIdToMostSpecificRegionId[regionID],
+		timeRangeStart,
+		timeRangeEnd,
+		numberDays,
+		true, minFreeKilometerWidth)
+	if err != nil {
+		log.Error("Unable to fetch free kilometer range", err)
+		return dto
+	}
+	defer rows.Close()
+
+	var freeKilometerRanges []models.FreeKilometerRange
+	for rows.Next() {
+		var freeKilometerRange models.FreeKilometerRange
+		err = rows.Scan(&freeKilometerRange.Start, &freeKilometerRange.End, &freeKilometerRange.Count)
+		if err != nil {
+			log.Error("Unable to scan free kilometer range", err)
+			return dto
+		}
+		freeKilometerRanges = append(freeKilometerRanges, freeKilometerRange)
+	}
+
+	// collect vollkasko count
+	rows, err = p.Db.Query(ctx, vollkaskoCountQuery, regionIdToMostSpecificRegionId[regionID],
+		timeRangeStart,
+		timeRangeEnd,
+		numberDays,
+		true)
+	if err != nil {
+		log.Error("Unable to fetch vollkasko count", err)
+		return dto
+	}
+	defer rows.Close()
+
+	var vollkaskoCount models.VollkaskoCount
+	for rows.Next() {
+		var hasVollkasko bool
+		var count uint64
+		err = rows.Scan(&hasVollkasko, &count)
+		if err != nil {
+			log.Error("Unable to scan vollkasko count", err)
+			return dto
+		}
+		if hasVollkasko {
+			vollkaskoCount.TrueCount = count
+		} else {
+			vollkaskoCount.FalseCount = count
+		}
+	}
+
+	return models.DTO{Offers: offers, PriceRanges: priceRanges, CarTypeCounts: carTypeCount, SeatsCount: seatsCountList, FreeKilometerRange: freeKilometerRanges, VollkaskoCount: vollkaskoCount}
 }
 
 // Helper to parse sortOrder
@@ -300,4 +557,5 @@ var regionIdToMostSpecificRegionId = map[uint64][]uint64{
 	57:  {57},
 	20:  {56, 57},
 	6:   {121, 122, 123, 124, 56, 57},
+	0: {58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 56, 57},
 }
